8afb83772931547bd6b0cc5b97d32ffaff11d11e
Rodrigo Reis <rodrigreis@gmail.com>
2022-11-26 12:39:42 -0300

    Update tests config for celery

--- app/settings/tests.py
+++ app/settings/tests.py
@@ -7,5 +7,5 @@ DATABASES["default"]["NAME"] = f"test_{default_db_name}"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DEFAULT_FROM_EMAIL = "webmaster@example.com"
 
-CELERY_BROKER_URL = "memory"
-CELERY_RESULT_BACKEND = "memory"
+CELERY_BROKER_URL = ""
+CELERY_RESULT_BACKEND = ""
